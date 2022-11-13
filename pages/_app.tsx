c6f4e4dc3e3aa8dd6e1fc521d33f6fa792bf40c3
Reses <reseflix@hotmail.com>
2022-11-13 15:54:03 -0300

    Correção para o github pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import BackgroundImage from '../public/backgroundCity.jpg'
+import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Image src={BackgroundImage} alt='background' fill/>
+  <Component {...pageProps}/>
+  </>
 }
